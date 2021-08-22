8e9073623e15ba443626384e718b8afc053467c3
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-08-22 14:50:53 -0700

    Created product context for initial DB

---
+++ OriginalHouseOfDonuts/Data/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OriginalHouseOfDonuts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OriginalHouseOfDonuts.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
