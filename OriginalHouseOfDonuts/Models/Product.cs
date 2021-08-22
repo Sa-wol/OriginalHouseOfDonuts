80310ec080f582e789b121978c3f5d5479086fab
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-08-22 14:51:54 -0700

    Added product model

---
+++ OriginalHouseOfDonuts/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OriginalHouseOfDonuts.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
