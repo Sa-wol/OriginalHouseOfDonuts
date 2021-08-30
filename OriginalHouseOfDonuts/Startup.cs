a0ca46b78270b52a40dee200440c5df50c74e1ea
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-08-29 20:01:48 -0700

    Deleted photo field in product class

--- OriginalHouseOfDonuts/Startup.cs
+++ OriginalHouseOfDonuts/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OriginalHouseOfDonuts.Data;
+using OriginalHouseOfDonuts.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,8 @@ namespace OriginalHouseOfDonuts
             services.AddControllersWithViews();
 
             services.AddDbContext<ProductContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IProductRepo, EFProductRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
