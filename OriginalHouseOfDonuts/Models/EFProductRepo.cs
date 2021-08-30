3f263ad7c7295f00b68fb3384e2fa1e84f72cf5b
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-08-29 20:00:51 -0700

    Created EFProductRepo and IProductRepo for EF DB

---
+++ OriginalHouseOfDonuts/Models/EFProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using OriginalHouseOfDonuts.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OriginalHouseOfDonuts.Models
+{
+    public class EFProductRepo : IProductRepo
+    {
+        private ProductContext _context;
+
+        public EFProductRepo(ProductContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
---
+++ OriginalHouseOfDonuts/Models/IProductRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OriginalHouseOfDonuts.Models
+{
+    public interface IProductRepo
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
