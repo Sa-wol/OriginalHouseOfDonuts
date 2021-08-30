f6d9c0a25cce61c1d868ee92bd172bac279ee32e
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-08-29 20:34:49 -0700

    Add migration to add product to prodcut context and DB

--- OriginalHouseOfDonuts/Migrations/20210830031833_eCommerceUpdate.Designer.cs
+++ OriginalHouseOfDonuts/Migrations/20210830031833_eCommerceUpdate.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OriginalHouseOfDonuts.Data;
 
 namespace OriginalHouseOfDonuts.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20210830031833_eCommerceUpdate")]
+    partial class eCommerceUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
